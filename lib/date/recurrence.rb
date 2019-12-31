8730497a53294732790d8fbc6171078b6a2f15e8
Robert Ferney <rob@ferney.org>
2019-12-31 11:23:56 -0700

    saving work

---
+++ lib/date/recurrence.rb
@@ -0,0 +1,8 @@
+require "date/recurrence/version"
+
+class Date
+  module Recurrence
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/date/recurrence/version.rb
@@ -0,0 +1,5 @@
+class Date
+  module Recurrence
+    VERSION = "0.1.0"
+  end
+end
